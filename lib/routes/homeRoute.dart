7e23f1ed45dd38e796ddbf8ce6d0f83ba4226b84
蔡潮森 <>
2020-03-02 16:48:49 +0800

    完成我的博客模块

--- lib/routes/homeRoute.dart
+++ lib/routes/homeRoute.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeRoute extends StatefulWidget {
   @override
@@ -9,6 +12,84 @@ class _HomeRouteState extends State<HomeRoute> with SingleTickerProviderStateMix
 
   final List<String> tabs = <String>['分类1','分类2','分类4','分类3'];
   TabController _controller;
+
+  Widget _getRichText(String normalText,String highText,GestureTapCallback onTap) {
+    return   Text.rich(TextSpan(
+                text: normalText,
+                children: [
+                  TextSpan(
+                    text: highText,
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                                ..onTap = (){
+                                  print('click me');
+                                }),],));
+  }
+
+  Widget _getlistView (){
+    return ListView.builder(
+        itemCount: 100,
+        itemExtent: 90,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Container(
+              margin: EdgeInsets.all(8),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '风科技的了房间爱上方方达冷风机阿里卷达冷风机阿里达冷风机阿里发梳发送',
+                  textAlign: TextAlign.left,
+                  softWrap: false,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                  ),
+                  ),
+                Text('描述'),
+
+                Row(
+                  children: <Widget>[
+                    _getRichText('分类：','哈哈',(){
+                                      print('click me');
+                                    }),
+                    _getRichText('  标签：','哈哈',(){
+                      print('click me');
+                    }),
+                    _getRichText('  作者：','哈哈',(){
+                      print('click me');
+                    }),
+                  ],
+                ),
+                ]
+            ),
+            )
+          );
+        }
+        );
+  }
+
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async{
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async{
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    // items.add((items.length+1).toString());
+    _refreshController.loadComplete();
+  }
 
   @override
   void initState() {
@@ -26,6 +107,39 @@ class _HomeRouteState extends State<HomeRoute> with SingleTickerProviderStateMix
           tabs: tabs.map((e) => Tab(text: e,)).toList(),
         ),
       ),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        header: WaterDropHeader(),
+        footer: CustomFooter(
+          builder: (BuildContext context,LoadStatus mode){
+            Widget body ;
+            if(mode==LoadStatus.idle){
+              body =  Text("");
+            }
+            else if(mode==LoadStatus.loading){
+              body =  CupertinoActivityIndicator();
+            }
+            else if(mode == LoadStatus.failed){
+              body = Text("Load Failed!Click retry!");
+            }
+            else if(mode == LoadStatus.canLoading){
+                body = Text("release to load more");
+            }
+            else{
+              body = Text("No more Data");
+            }
+            return Container(
+              height: 55.0,
+              child: Center(child:body),
+            );
+          },
+        ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: _getlistView(),
+        )
     );
   }
 }
--- lib/routes/myBolgRoute.dart
+++ lib/routes/myBolgRoute.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:loading_animations/loading_animations.dart';
 
-class MyBolgRoute extends StatefulWidget {
+class MyBolgRoute extends StatefulWidget  {
   @override
   _MyBolgRouteState createState() => _MyBolgRouteState();
 }
 
 class _MyBolgRouteState extends State<MyBolgRoute> {
+
+  var _isLoading = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('我的博客'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('我的博客'),
+      ),
+      body: 
+      Stack(
+        children: <Widget>[
+                 WebView(
+        initialUrl: 'http://www.baidu.com',
+        javascriptMode: JavascriptMode.unrestricted,
+        onPageFinished:(url){
+          setState(() {
+            _isLoading = false;
+          });
+          print('$url 加载完成');
+        },
+        onPageStarted:(url){
+          print('$url 开始加载');
+        },
+      ),
+      _isLoading ? LoadingFlipping.circle() : Container(
+        constraints: BoxConstraints(maxWidth: 0.01,maxHeight: 0.01),
+        ),
+        ],
+      )
     );
   }
 }
